8e27e41613ee9c7e5680c57f69c7dcf372125ba0
Robin Reyes <ext_rre@imercoas.onmicrosoft.com>
2026-04-28 01:38:45 +0800

    Remove ipsec saref

--- backend/vpn/l2tp.js
+++ backend/vpn/l2tp.js
@@ -22,7 +22,6 @@ const L2TPManager = {
     shell.mkdir('-p', '/var/run/xl2tpd');
     shell.mkdir('-p', '/etc/xl2tpd');
     shell.touch('/etc/xl2tpd/l2tp-secrets'); 
-    shell.echo('* * *').to('/etc/xl2tpd/l2tp-secrets'); // Wildcard for all tunnel auth
     shell.exec('/usr/sbin/xl2tpd -c /etc/xl2tpd/xl2tpd.conf', { silent: true });
   },
 
